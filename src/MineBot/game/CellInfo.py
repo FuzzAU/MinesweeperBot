8716ae2057d1376a8f2bb30c74632b791967a6b0
Andrew Walker <ab.walker@optusnet.com.au>
2011-12-10 21:16:32 +1100

    reindented CellInfo, removed redundant static variables

--- src/MineBot/game/CellInfo.py
+++ src/MineBot/game/CellInfo.py
@@ -1,17 +1,16 @@
 
 class CellInfo(object):
-  hasMine = False
-  surroundingMines = 0
+    def __init__( self ):
+        self.hasMine = False
+        self.surroundingMines = 0
 
-  def __init__( self ):
-    self.hasMine = False
-    self.surroundingMines = 0
-
-  # Get an ASCII character representation of this cell
-  def GetChar( self ):
-    cellChar = '0'
-    if self.hasMine == True:
-        cellChar = 'X'
-    else:
-        cellChar = str( self.surroundingMines )
-    return cellChar
+    def GetChar( self ):
+        """
+        Get an ASCII character representation of this cell
+        """
+        cellChar = '0'
+        if self.hasMine == True:
+            cellChar = 'X'
+        else:
+            cellChar = str( self.surroundingMines )
+        return cellChar
