eb59521e473cb4257089f4b04ae7757de2ec3a8b
Justin Young <fuzz.young@gmail.com>
2011-12-11 01:19:06 +1100

    Added a function to count adjacement mines

--- MineBot/game/MineGame.py
+++ MineBot/game/MineGame.py
@@ -29,6 +29,28 @@ class MineGame(object):
             self.mineGrid.append(row)
 
         MinePlacer.placeMines(self.mineGrid, numberOfMines)
+        self.PerformMineCount()
+
+    def PerformMineCount(self):
+        for x in xrange(len(self.mineGrid)):
+            for y in xrange(len(self.mineGrid[0])):
+                self.CountAdjacentMines(x,y)
+
+    def CountAdjacentMines(self, mineX, mineY):
+        cell = self.mineGrid[mineX][mineY]
+        # Maybe I need to make the mine grid something smarter???
+        size = ( len(self.mineGrid), len(self.mineGrid[0]) )
+        for xAdj in xrange(mineX-1, mineX+2):
+            # Do bounds check for X
+            if (xAdj < 0) | (xAdj > size[0] - 1):
+                continue
+            for yAdj in xrange(mineY-1, mineY+2):
+                # Do bounds check for Y
+                if (yAdj < 0) | (yAdj > size[1] - 1):
+                    continue
+                testCell = self.mineGrid[xAdj][yAdj]
+                if testCell.hasMine == True:
+                    cell.surroundingMines += 1 
 
     def DisplayGrid(self):
         for line in self.mineGrid:
