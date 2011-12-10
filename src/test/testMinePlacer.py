ce80f2adc11f2d6f244f74a8c6a9371c9b4594b7
Andrew Walker <ab.walker@optusnet.com.au>
2011-12-10 22:45:17 +1100

    improving mine placer tests

--- src/test/testMinePlacer.py
+++ src/test/testMinePlacer.py
@@ -22,11 +22,22 @@ class MinePlacementTests(unittest.TestCase):
         mines = MinePlacer.getRandomMines(cells, 50)
         self.assertEquals(len(mines), 25)
 
+    def testMineCellsUnique(self):
+        # This test is stochastic, but the probability of failing
+        # is exceptionally low, provided that the code works as
+        # designed
+
+        cells = MinePlacer.generateList(5,5)
+        mines = MinePlacer.getRandomMines(cells, 25)
+        # sanity test
+        self.assertEquals(len(mines),25)
+
+        # NB. a mine is a list ATM, list are not hashable
+        #     so we transform them into tuples which can
+        #     be hashed
+        mines = [ tuple(mine) for mine in mines ]
+        mines = set(mines)
+        self.assertEquals(len(mines),25)
 
 
-#def test():
-#    myList = MinePlacer.generateList(5, 5)
-#    MinePlacer.printCellList(myList)
-#    mines = MinePlacer.placeMines(myList, 10)
-#    print 'Mines:'
-#    MinePlacer.printCellList(mines)
+
