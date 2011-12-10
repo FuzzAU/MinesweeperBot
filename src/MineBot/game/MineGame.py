f82c14d8339151a6729d49457fdb330df970d20d
Andrew Walker <ab.walker@optusnet.com.au>
2011-12-10 23:02:52 +1100

    Improvements and bug fixes in the MineGame class
    Fixed bug where __init__ was not initialising members correctly
    Added helper function to get flattened list of cells
    Bug fix in the way in which the list of cells are generated

--- src/MineBot/game/MineGame.py
+++ src/MineBot/game/MineGame.py
@@ -6,10 +6,27 @@ import sys
 class MineGame(object):
 
     def __init__(self):
-        mineGrid = []
+        self.mineGrid = []
+
+    def Cells(self):
+        """
+        Return a flattened list of cells
+        """
+        res = []
+        for row in self.mineGrid:
+            for item in row:
+                res.append(item)
+        return res
 
     def InitGame(self, xSize, ySize, numberOfMines):
-        self.mineGrid = [[CellInfo()] * ySize for x in xrange(xSize)]
+        # TODO - JY to remove following line after experimenting with
+        #        MineGameTests.testCellInfoUniqueness
+        #self.mineGrid = [[CellInfo()] * ySize for x in xrange(xSize)]
+        for j in xrange(ySize):
+            row = []
+            for i in xrange(xSize):
+                row.append(CellInfo())
+            self.mineGrid.append(row)
 
         MinePlacer.placeMines(self.mineGrid, numberOfMines)
 
