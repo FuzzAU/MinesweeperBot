7ca99c8472326ceb10606f49393caba7128a2e5e
Justin Young <fuzz.young@gmail.com>
2011-12-10 17:58:01 +1100

    Added a module to generate a list of cells, and make random mine locations

---
+++ MinePlacer.py
@@ -0,0 +1,32 @@
+import random
+
+# Generate a list of all cells in a grid of sizeX by sizeY
+def generateList( sizeX, sizeY ):
+    cellList = []
+    for x in range( 0, sizeX ):
+        for y in range( 0, sizeY ):
+            cellList.append( [ x,y ] )
+    return cellList
+
+# For debugging, print a list of all cells in the provided list
+def printCellList( cellList ):
+    for cell in cellList:
+        print cell
+
+# Will generate random mine locations from the provided list of cells
+def placeMines( cellList, numberOfMines ):
+    mineLocations = []
+    cellListCopy = cellList
+
+    for mineNum in range( 0, numberOfMines ):
+        randIndex = random.randint( 0, len( cellListCopy ) - 1 )
+        mineLocations.append( cellListCopy.pop( randIndex ) )
+
+    return mineLocations
+
+def testMinePlacement():
+    myList = generateList( 5, 5 )
+    printCellList( myList )
+    mines = placeMines( myList, 10 )
+    print 'Mines:'
+    printCellList( mines )
